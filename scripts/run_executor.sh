36f1fab45a69ddfb0814dcc5b3e8c97907031fd7
jodafons <jodafons@lps.ufrj.br>
2023-09-22 15:58:24 -0300

    update

--- scripts/run_executor.sh
+++ scripts/run_executor.sh
@@ -1,16 +1,6 @@
 
 
-
-export PILOT_SERVER_HOST="http://maestro-server.lps.ufrj.br:5003"
-export EXECUTOR_SERVER_HOST="http://${HOSTNAME}:5000"
-export EXECUTOR_SERVER_BINDS="{'/home':'/home', '/mnt/cern_data':'/mnt/cern_data'}"
-export EXECUTOR_SERVER_DEVICE="-1"
-export EXECUTOR_SERVER_SLOT_SIZE="1"
-export EXECUTOR_PARTITION="cpu"
-
-
 REPO_DIR=$(mktemp -d)
-
 CURRENT_DIR=$PWD
 cd $REPO_DIR
 echo "download repository..."
@@ -19,7 +9,5 @@ git checkout dev
 source envs.sh
 make build_local
 make start
-
-
 cd $CURRENT_DIR
 
