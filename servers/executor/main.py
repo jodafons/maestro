a66b2f10e01af7b25cbeebe0ad60dc1943d03637
jodafons <jodafons@lps.ufrj.br>
2023-09-22 23:18:40 -0300

    update

--- servers/executor/main.py
+++ servers/executor/main.py
@@ -94,4 +94,4 @@ async def update( describe : Describe ) :
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
