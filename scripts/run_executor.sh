18ca4f58ca31affef3ce71fddb11614050c727f9
jodafons <jodafons@lps.ufrj.br>
2023-09-22 21:52:55 -0300

    update

--- scripts/run_executor.sh
+++ scripts/run_executor.sh
@@ -7,8 +7,12 @@ cd $REPO_DIR
 echo "download repository..."
 git clone https://github.com/jodafons/orchestra-server.git && cd orchestra-server
 git checkout dev
-ls -lisah
+
+export VIRTUALENV_NAMESPACE=orchestra-env
+export MAESTRO_PATH=$PWD
 export PYTHONPATH=$PWD:$PYTHONPATH
+
+ls -lisah
 make build_local
 make start
 cd $CURRENT_DIR
