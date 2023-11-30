912dba63eaf054cf8a8473b4596279fb25aef8dd
jodafons <jodafons@lps.ufrj.br>
2023-11-30 08:09:41 -0300

    update

---
+++ scripts/maestro.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import sys, os, argparse
+from maestro.parsers import *
+
+def main():
+
+  parser = argparse.ArgumentParser()
+  commands = parser.add_subparsers(dest='mode')
+
+  parsers = [
+              task_parser(commands),
+              data_parser(commands),
+              run_parser(commands),
+            ]
+
+  if len(sys.argv)==1:
+    print(parser.print_help())
+    sys.exit(1)
+
+  args = parser.parse_args()
+
+  # Run!
+  for p in parsers:
+    p.parser(args)
+
+
+
+if __name__ == "__main__":
+  main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
