4831129ea846321d051545605a6c999ec1e0c08f
Joao Pinto <joao.pinto1@dell.com>
2023-12-05 14:27:43 +0000

    update

--- maestro/servers/control/main.py
+++ maestro/servers/control/main.py
@@ -51,7 +51,7 @@ def run( args , launch_executor : bool=False ):
     pilot      = Pilot(pilot_url, schedule)
 
     # mlflow tracking server
-    tracking   = Server( f"mlflow ui --port {args.tracking_port} --backend-store-uri {args.tracking_location}/mlflow  --artifacts-destination {args.tracking_location}/artifacts" )
+    tracking   = Server( f"mlflow ui --port {args.tracking_port} --host 0.0.0.0 --backend-store-uri {args.tracking_location}/mlflow  --artifacts-destination {args.tracking_location}/artifacts" )
     
     if launch_executor:
         executor = Server(f"maestro run executor --max_procs {args.max_procs} --device {args.device} --partition {args.partition} --executor-port {args.executor_port} --database-url {args.database_url}")
