1852fc0ce94cc4b2c7c84946c197887b188e0f9a
jodafons <jodafons@lps.ufrj.br>
2023-12-11 11:13:31 -0300

    up

--- tests/test_standalone/task.standalone/job.sort_1/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-cd /home/joao_pinto/my_github/orchestra-server/tests/test_standalone/task.standalone/job.sort_1
-source /home/joao_pinto/my_github/orchestra-server/maestro-env/bin/activate
-python /home/joao_pinto/my_github/orchestra-server/tests/test_standalone/test_local.py -j /home/joao_pinto/my_github/orchestra-server/tests/test_standalone/jobs/job.sort_1.json
--- tests/test_standalone/task.standalone/job.sort_4/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-cd /home/joao_pinto/my_github/orchestra-server/tests/test_standalone/task.standalone/job.sort_4
-source /home/joao_pinto/my_github/orchestra-server/maestro-env/bin/activate
-python /home/joao_pinto/my_github/orchestra-server/tests/test_standalone/test_local.py -j /home/joao_pinto/my_github/orchestra-server/tests/test_standalone/jobs/job.sort_4.json
